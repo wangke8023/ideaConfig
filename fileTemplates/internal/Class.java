8204383ba1eeaeaba861d4dbac4b6440bd07ac28
wangke <527767731@qq.com>
2019-07-31 16:55:36 +0800

    IU-2019.2 <ke.wang01@SZXH3FITWB050 Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, other.xml, git.xml, _@user_High ontrast.icls, project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, find.xml, ui.lnf.xml, dataSources.xml, filetypes.xml, projectView.xml, keymap.xml, javaeeExternalResources.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, databaseDrivers.xml, templates.xml, AnnotationType.java, Obsidian.icls, output.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, intentionSettings.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**  
+ * @ProjectName ${PROJECT_NAME}
+ * @Title: ${NAME}
+ * @Description: TODO
+ * @author ${USER}
+ * @date ${DATE} ${TIME}
+*/
+public class ${NAME} {
+}
